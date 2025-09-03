75b2b7f7494044d578a9b89dd42bd108aae327be
VicMelRavi <104226145+VicMelRavi@users.noreply.github.com>
2025-09-03 19:29:28 +1000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,16 +71,21 @@ data "aws_availability_zones" "available" {
 # Data source for latest Ubuntu AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"]
+  owners      = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-24.04-amd64-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 }
 
